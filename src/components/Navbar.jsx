96026b5c63e5cb3e4a73fdfbeb37dd81c4c9ac1a
uCodak <uchecyprain97@gmail.com>
2026-03-23 17:38:12 +0100

    add routing and navigation to Navbar; include react-router-dom dependency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import lastbarlogo from "../assets/lastbarlogo.png";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   const [scrollUp, setScrollUp] = useState(false);
@@ -22,20 +23,46 @@ function Navbar() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  const navItems = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/about" },
+    { name: "Services", path: "/services" },
+    { name: "Contact", path: "/contact" },
+  ];
+
   return (
     <>
       <nav
         className={`
         fixed top-0 left-0 w-full z-50 transition-colors duration-300 py-1
-        ${atTop ? "bg-white/20 backdrop-blur-md" : ""}
+        ${atTop ? "bg-[#333333] backdrop-blur-lg" : ""}
         ${scrollUp ? "bg-white/10  backdrop-blur-lg" : ""}
       `}
       >
-        <div className="w-[90%] mx-auto">
-          <img src={lastbarlogo} alt="" width={210} className="" />
+        <div className="flex items-center justify-between w-[90%] mx-auto">
+          <div className="">
+            <img src={lastbarlogo} alt="" width={210} className="" />
+          </div>
+          <div className="roboto">
+            {navItems.map((item, index) => (
+              <NavLink
+                key={index}
+                to={item.path}
+                className={({ isActive }) =>
+                  `text-sm font-medium transition ${
+                    isActive ? "text-orange-500" : "text-gray-300"
+                  }`
+                }
+              >
+                {item.name}
+              </NavLink>
+            ))}
+          </div>
         </div>
       </nav>
-      <div className="bg-black h-screen mb-250">d</div>
+      <div className="bg-black h-screen mb-250 text-white mt-15">
+        dfdfdfdfdfdfdfdf
+      </div>
       <p>ffgfgfgfg</p>
     </>
   );
